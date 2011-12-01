9f71a38c591798a3ef5289c01eb80dd5d91d477f
Peter Malcolm <peter.t.malcolm@gmail.com>
2011-12-01 17:59:48 -0500

    this is the REAL initializing commit for the livePortalbranch

--- src/main/java/org/telscenter/sail/webapp/dao/portal/PortalStatisticsDao.java
+++
@@ -1,9 +0,0 @@
-package org.telscenter.sail.webapp.dao.portal;
-
-import org.telscenter.sail.webapp.domain.portal.PortalStatistics;
-
-import net.sf.sail.webapp.dao.SimpleDao;
-
-public interface PortalStatisticsDao<T extends PortalStatistics> extends SimpleDao<T> {
-
-}
